663cb126e40e8cca891cadab5fbf1d6ea84a4e98
NhungNguyen <nhungnguyen10122002@gmail.com>
2022-10-18 12:49:36 +0700

    first commit

---
+++ SinhVien.py
@@ -0,0 +1,7 @@
+class SinhVien:
+    def __init__(self, id, name, sex, age) -> None:
+        pass
+        self._id = id
+        self._name = name
+        self._sex = sex
+        self._age = age
